2ba50c89be0316b5b08f503f6e725da6a7b540f6
prince unachukwu <prinxard@gmail.com>
2022-03-02 10:08:34 +0100

    Fixed payment issues

--- src/config/url.js
+++ src/config/url.js
@@ -5,7 +5,7 @@ const url = {
 
   // live db
   BASE_URL: "https://portallive.bespoque.dev/api/v1/",
-  // PAY_URL: "https://etaxpay.irs.kg.gov.ng/",
+  PAY_URL: "https://etaxpay.irs.kg.gov.ng/",
 
   // Test Db
   // BASE_URL: "https://portalapi.bespoque.dev/api/v1/",
