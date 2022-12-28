ca2dcac0c7e26db954da8589e9ad05ee261ed91b
prince unachukwu <prinxard@gmail.com>
2022-12-28 11:02:10 +0100

    refactored annual filing menu

--- src/pages/uploads/annual.js
+++
@@ -1,4 +0,0 @@
-import AnnualUploadForm from "../../components/Form/AnnualUpload";
-
-const Index = () => <AnnualUploadForm />;
-export default Index;
--- src/pages/uploads/annualdocs.js
+++
@@ -1,4 +0,0 @@
-import AnnualDocsUploadForm from "../../components/Form/AnnualDocsUpload";
-
-const Index = () => <AnnualDocsUploadForm />;
-export default Index;
