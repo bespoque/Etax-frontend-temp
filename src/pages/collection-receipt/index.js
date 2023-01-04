5a58901d6daf7f4923a3ae3ac4a037a63727565f
prince unachukwu <prinxard@gmail.com>
2023-01-04 14:14:56 +0100

    added decription to receipt

--- src/pages/collection-receipt/index.js
+++ src/pages/collection-receipt/index.js
@@ -78,7 +78,7 @@ export default function index() {
               </button>
               <div >
                 <ReactToPrint
-                  // pageStyle='@page { size: auto; margin: 0mm; } @media print { body { -webkit-print-color-adjust: exact; padding: 40px !important; } }'
+                  pageStyle='@page { size: auto; margin: 0mm; } @media print { body { -webkit-print-color-adjust: exact; padding: 40px !important; } }'
                   // pageStyle="@page { size: 7.5in 13in  }"
                   trigger={() => <button className="btn w-32 bg-green-600 btn-default text-white
                   btn-outlined bg-transparent rounded-md"
@@ -90,38 +90,38 @@ export default function index() {
                 />
               </div>
             </div>
-            <div class="border p-6" ref={componentRef}>
+            <div className="border p-6" ref={componentRef}>
               <p>KOGI STATE GOVERNMENT</p>
-              <section class="flex justify-between">
-                <p class="font-bold">REVENUE RECEIPT</p>
-                <p class="font-bold">{`Ref - ${colData.referenceNo}`}</p>
+              <section className="flex justify-between">
+                <p className="font-bold">REVENUE RECEIPT</p>
+                <p className="font-bold">{`Ref - ${colData.referenceNo}`}</p>
               </section>
-              <section class="flex justify-end mt-8">
+              <section className="flex justify-end mt-8">
                 <CoatOfArms />
                 <KogiGov />
                 <KgirsLogo2 />
               </section>
-              <div class="flex justify-between">
+              <div className="flex justify-between">
                 <div>
-                  <div class="grid grid-cols-6 gap-2">
+                  <div className="grid grid-cols-6 gap-2">
                     <p>PAID BY:</p>
-                    <p class="font-bold col-span-2">{colData.taxpayerName}</p>
+                    <p className="font-bold col-span-2">{colData.taxpayerName}</p>
                   </div>
-                  <div class="grid grid-cols-6 gap-2">
+                  <div className="grid grid-cols-6 gap-2">
                     <p>PAYER ID:</p>
-                    <p class="font-bold col-span-2">
+                    <p className="font-bold col-span-2">
                       {colData.taxpayerId}
                     </p>
                   </div>
-                  <div class="grid grid-cols-6 gap-2">
+                  <div className="grid grid-cols-6 gap-2">
                     <p>ADDRESS:</p>
-                    <p class="font-bold col-span-2">{colData.taxpayerAddress}</p>
+                    <p className="font-bold col-span-2">{colData.taxpayerAddress}</p>
                   </div>
-                  <div class="flex mt-10">
-                    <div class='w-16 border-b-2'>
+                  <div className="flex mt-10">
+                    <div className='w-16 border-b-2'>
                     </div>
-                    <p class='align-self-center'>Details</p>
-                    <div class="border-b-2 w-3/4 ">
+                    <p className='align-self-center'>Details</p>
+                    <div className="border-b-2 w-3/4 ">
                     </div>
                   </div>
                 </div>
@@ -131,50 +131,61 @@ export default function index() {
                     size={120}
                   />
                 </div>
-
               </div>
-              <div class="mt-3">
-                <div class="grid grid-cols-6 gap-2">
+              <div className="mt-3">
+                <div className="grid grid-cols-6 gap-2">
                   <p>PAYMENT DATE:</p>
-                  <p class="font-bold col-span-2">{colData.transactionDate}</p>
+                  <p className="font-bold col-span-2">{colData.transactionDate}</p>
                 </div>
-                <div class="grid grid-cols-6 gap-2">
+                <div className="grid grid-cols-6 gap-2">
                   <p>AMOUNT:</p>
-                  <div class="col-span-4">
-                    <p class="font-bold">NGN {formatNumber(colData.amount)}</p>
+                  <div className="col-span-4">
+                    <p className="font-bold">NGN {formatNumber(colData.amount)}</p>
 
                   </div>
                 </div>
-                <div class="grid grid-cols-6 gap-2">
+                <div className="grid grid-cols-6 gap-2">
                   <p>BEING:</p>
-                  <div class="col-span-3">
-                    <p class="font-bold"> {`Payment for ${colData.revenueCode}`} </p>
+                  <div className="col-span-3">
+                    <p className="font-bold"> {`Payment for ${colData.revenueCode}`} </p>
                     <small>
                       {colData.revenueItem}
                     </small>
                   </div>
                 </div>
-                <div class="grid grid-cols-6 gap-2">
-                  <p>PAID AT:</p>
-                  <p class="font-bold"> {colData.bank} </p>
-                </div>
-                <div class="grid grid-cols-6 gap-2">
-                  <p>AGENCY:</p>
-                  <div class="col-span-3">
-                    <p class="font-bold"> INTERNAL REVENUE SERVICE </p>
+                <div className="grid grid-cols-6 gap-2">
+                  <p>DESCRIPTION:</p>
+                  <div className="col-span-3">
+                    <p className="font-bold"> {colData.description} </p>
                   </div>
                 </div>
-                <div class="grid grid-cols-6 gap-2">
-                  <p>TAX STATION:</p>
-                  <p class="font-bold"> {colData.taxOffice} </p>
+                <div className="grid grid-cols-6 gap-2">
+                  <p>DETAILS:</p>
+                  <div className="col-span-3">
+                    <p className="font-bold"> {colData.details} </p>
+                  </div>
                 </div>
-                <div class="border-b-2 mt-3 w-4/4 ">
+                <div className="grid grid-cols-6 gap-2">
+                  <p>PAID AT:</p>
+                  <p className="font-bold"> {colData.bank} </p>
+                </div>
+                <div className="grid grid-cols-6 gap-2">
+                  <p>AGENCY:</p>
+                  <div className="col-span-3">
+                    <p className="font-bold"> INTERNAL REVENUE SERVICE </p>
+                  </div>
+                </div>
+                <div className="grid grid-cols-6 gap-2">
+                  <p>TAX STATION:</p>
+                  <p className="font-bold"> {colData.taxOffice} </p>
+                </div>
+                <div className="border-b-2 mt-3 w-4/4 ">
                 </div>
               </div>
 
-              <div class="flex justify-between">
+              <div className="flex justify-between">
                 <div></div>
-                <div class="mt-2">
+                <div className="mt-2">
                   <Signature />
                   <hr />
                   Authorized Signatory
