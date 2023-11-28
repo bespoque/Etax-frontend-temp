02238afe0004a9b70c0d62a35f9b2f031fcadbc4
prinxard <prinxard@gmail.com>
2023-11-28 10:24:59 +0100

    update

--- src/config/url.js
+++ src/config/url.js
@@ -4,8 +4,8 @@ const url = {
   // PAY_URL: 'http://localhost/etaxpay/',
 
   // live db
-  // BASE_URL: "https://portallive.bespoque.dev/api/v1/",
-  BASE_URL: "https://rhmapi.irs.kg.gov.ng/api/v1/",
+  BASE_URL: "https://portallive.bespoque.dev/api/v1/",
+  // BASE_URL: "https://rhmapi.irs.kg.gov.ng/api/v1/",
   PAY_URL: "https://etaxpay.irs.kg.gov.ng/",
 
   // Test Db
